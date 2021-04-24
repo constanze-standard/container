f2526917a97d8bb45fc2efe6d9a4e1d2cef259d6
alex-1900 <omytty@126.com>
2021-04-25 00:52:28 +0800

    update abstract test file

--- tests/AbstractTest.php
+++ tests/AbstractTest.php
@@ -13,7 +13,7 @@ abstract class AbstractTest extends TestCase
      */
     protected function setProperty($obj, $property, $value)
     {
-        $reflectionProperty = new \ReflectionProperty($obj, $property);
+        $reflectionProperty = new ReflectionProperty($obj, $property);
         $reflectionProperty->setAccessible(true);
         $reflectionProperty->setValue($obj, $value);
         $reflectionProperty->setAccessible(false);
@@ -24,7 +24,7 @@ abstract class AbstractTest extends TestCase
      */
     protected function getProperty($obj, $property)
     {
-        $reflectionProperty = new \ReflectionProperty($obj, $property);
+        $reflectionProperty = new ReflectionProperty($obj, $property);
         $reflectionProperty->setAccessible(true);
         $value = $reflectionProperty->getValue($obj);
         $reflectionProperty->setAccessible(false);
@@ -36,7 +36,7 @@ abstract class AbstractTest extends TestCase
      */
     protected function callMethod($instance, $methodName, $args = [])
     {
-        $method = new \ReflectionMethod($instance, $methodName);
+        $method = new ReflectionMethod($instance, $methodName);
         $method->setAccessible(true);
         $result = $method->invokeArgs($instance, $args);
         $method->setAccessible(false);
